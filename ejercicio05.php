9e27950172cc0e840ff1cc4e6dfa07150326115a
luis1184lopez <62674480+luis1184lopez@users.noreply.github.com>
2020-11-17 03:09:04 +0100

    Create ejercicio05.php

---
+++ ejercicio05.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+  <head>
+    <title>Practice makes perfect!</title>
+    <link rel="icon" type="image/x-icon" href="./assets/favicon.ico" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+
+  <body>
+      <p>
+          <!--Your code goes here -->
+          <?php
+            class Dog{
+                //Atributtes 
+                public $numLegs=4;
+                public $name;
+                //Methods
+                public function  __construct ($name){
+                     $this->name = $name;
+                 }
+            }
+          ?>
+      </p>
+  </body>
+</html>
