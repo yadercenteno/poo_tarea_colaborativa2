460b4a9d2851dd2caa6a3c47313af209d23b7cd7
luis1184lopez <62674480+luis1184lopez@users.noreply.github.com>
2020-11-17 03:38:34 +0100

    Create ejercicio07.php

---
+++ ejercicio07.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+  <head>
+    <title>Reconstructing the Person Class</title>
+    <link rel="icon" type="image/x-icon" href="./assets/favicon.ico" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+
+  <body>
+      <p> 
+          <!--Your code goes here -->
+          <?php
+           class Person{
+
+           }
+        $teacher = new Person();
+        $student = new Person();
+
+          ?>
+      </p>
+  </body>
+</html>
